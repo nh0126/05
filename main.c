780fab69827797116c06687052d717f2dd80efa2
nh0126 <nh0126@sookmyung.ac.kr>
2023-10-08 23:27:42 +0900

    실습2
    실습2

--- main.c
+++ main.c
@@ -9,12 +9,10 @@ int main(int argc, char *argv[]) {
     printf("정수 하나를 입력하시오 : ");
     scanf("%d",&input);
 
-    if(input > 0)
-             printf("양수입니다.\n");
-    else if(input < 0)
-         printf("음수입니다.\n");
-    else 
-         printf("0 입니다.\n");
+    if(input < 0)
+        printf("절댓값은 %d 입니다.\n", input=-input);
+    else
+	    printf("절댓값은 %d 입니다.\n", input);
 
     system("PAUSE");
 	return 0;
